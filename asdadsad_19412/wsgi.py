f1d5bf5e2e67690ee5cf10fb0da5b89b59743c0d
Crowdbotics <team@crowdbotics.com>
2020-08-06 00:52:58 +0000

    Initial commit.

---
+++ asdadsad_19412/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for asdadsad_19412 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'asdadsad_19412.settings')
+
+application = get_wsgi_application()
